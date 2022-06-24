2bda71080ed1e68cf453d5b6424aef117a677e61
Abdella <abdella.shukur2@gmail.com>
2022-06-24 13:56:31 -0400

    implemented all features and art

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -41,10 +41,20 @@ public class Main {
             int value = random.nextInt(max+min)+min;
     
             try(Scanner obj = new Scanner(System.in)) {
-            System.out.print("Do you want to play easy, medium, or hard? (1-3) ");
+            System.out.println("Welcome Guest!");
+            System.out.print("Do you want to play on easy - 0, medium - 1, or hard - 2? (0-2) ");
             int userDifficulty = obj.nextInt();
+            System.out.println("You choose " + userDifficulty);
+           
+            String[] levels = {
+                "Easy - User wins if you guess higher or same as Computer", 
+                "Medium - User wins if you guess higher than computer",
+                "Hard - User wins if you guess exactly as computer"};
 
-            if(userDifficulty > 3 || userDifficulty < 1) {
+            System.out.println(levels[userDifficulty]);
+
+
+            if(userDifficulty > 2 || userDifficulty < 0) {
                 System.out.println("Pick a number within range!");
                 return;
             }
@@ -52,10 +62,10 @@ public class Main {
             System.out.print("Pick a number (0-10): ");
             int userNumber = obj.nextInt();
             
-            if(userNumber == value && userDifficulty == 1) {
+            if(userNumber == value && userDifficulty == 0) {
                 System.out.println("Your answer is the same. You Won!");
             }
-            else if (userNumber == value && userDifficulty == 2) {
+            else if (userNumber == value && userDifficulty == 1) {
                 System.out.println("Your answer is the same. Computer Wins!");
             }
             else if (userNumber < value) {
@@ -64,7 +74,7 @@ public class Main {
            else  if (userNumber > 10 && userNumber > value) {
                 System.out.println("Number outside range");
             }
-            else if(userNumber > value && userDifficulty == 3) {
+            else if(userNumber > value && userDifficulty == 2) {
                 System.out.println("Your answer was too high. Computer Wins!");
             }
             else {
@@ -72,7 +82,7 @@ public class Main {
             }
         }
         catch(Exception e) {
-            System.out.println("Make sure you type in a number!");
+            System.out.println("Type a postive number within range!");
         }
     }
 }
