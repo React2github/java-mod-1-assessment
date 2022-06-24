7965afd3357d097a0b962077af2c6630c880191c
Abdella <abdella.shukur2@gmail.com>
2022-06-24 12:53:00 -0400

    all features are working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,13 +9,23 @@ public class Main {
             Random random = new Random();
             int value = random.nextInt(max+min)+min;
     
-            Scanner obj = new Scanner(System.in);
+            try(Scanner obj = new Scanner(System.in)) {
+            System.out.print("Do you want to play easy, medium, or hard? (1-3) ");
+            int userDifficulty = obj.nextInt();
+
+            if(userDifficulty > 3 || userDifficulty < 1) {
+                System.out.println("Pick a number within range!");
+                return;
+            }
+
             System.out.print("Pick a number (0-10): ");
             int userNumber = obj.nextInt();
-    
-    
-            if(userNumber == value) {
-                System.out.println("Your answer is the same. Tied!");
+            
+            if(userNumber == value && userDifficulty == 1) {
+                System.out.println("Your answer is the same. You Won!");
+            }
+            else if (userNumber == value && userDifficulty == 2) {
+                System.out.println("Your answer is the same. Computer Wins!");
             }
             else if (userNumber < value) {
                 System.out.println("Your answer was too low. Computer Wins!");
@@ -23,8 +33,15 @@ public class Main {
            else  if (userNumber > 10 && userNumber > value) {
                 System.out.println("Number outside range");
             }
+            else if(userNumber > value && userDifficulty == 3) {
+                System.out.println("Your answer was too high. Computer Wins!");
+            }
             else {
                 System.out.println("Your answer was higher. You Win!");
             }
+        }
+        catch(Exception e) {
+            System.out.println("Make sure you type in a number!");
+        }
     }
 }
