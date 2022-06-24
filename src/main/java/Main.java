e5197109a4401cde6eef98e18768d217899c693e
Abdella <abdella.shukur2@gmail.com>
2022-06-24 11:14:58 -0400

    Most features are working now

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // your code here
+            int min = 0;
+            int max = 10;
+    
+            Random random = new Random();
+            int value = random.nextInt(max+min)+min;
+    
+            Scanner obj = new Scanner(System.in);
+            System.out.print("Pick a number (0-10): ");
+            int userNumber = obj.nextInt();
+    
+    
+            if(userNumber == value) {
+                System.out.println("Your answer is the same. Tied!");
+            }
+            else if (userNumber < value) {
+                System.out.println("Your answer was too low. Computer Wins!");
+            } 
+           else  if (userNumber > 10 && userNumber > value) {
+                System.out.println("Number outside range");
+            }
+            else {
+                System.out.println("Your answer was higher. You Win!");
+            }
     }
 }
