713fe384b18d5666ac8e4b9aa809e9ace7006025
Abdella <abdella.shukur2@gmail.com>
2022-06-24 14:02:15 -0400

    formated code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,22 +5,22 @@ import java.awt.image.BufferedImage;
 
 public class Main {
     public static void main(String[] args) {
-            int min = 0;
-            int max = 10;
-            int width = 100;
-            int height = 20;
+        int min = 0;
+        int max = 10;
+        int width = 100;
+        int height = 20;
 
-            // Buffered image 
-            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-            Graphics g = image.getGraphics();
-            g.setFont(new Font("TimesNewRoman", Font.BOLD, 10));
+        // Buffered image
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics g = image.getGraphics();
+        g.setFont(new Font("TimesNewRoman", Font.BOLD, 10));
 
-            Graphics2D graphics = (Graphics2D) g;
-            graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-            RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-            graphics.drawString("Guess the #", 20, 20);      
-            
-           //ImageIO.write(image, "png", new File("/users/mkyong/ascii-art.png"));
+        Graphics2D graphics = (Graphics2D) g;
+        graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        graphics.drawString("Guess the #", 20, 20);
+
+        // ImageIO.write(image, "png", new File("/users/mkyong/ascii-art.png"));
 
         for (int y = 0; y < height; y++) {
             StringBuilder sb = new StringBuilder();
@@ -37,51 +37,44 @@ public class Main {
             System.out.println(sb);
         }
 
-            Random random = new Random();
-            int value = random.nextInt(max+min)+min;
-    
-            try(Scanner obj = new Scanner(System.in)) {
+        Random random = new Random();
+        int value = random.nextInt(max + min) + min;
+
+        try (Scanner obj = new Scanner(System.in)) {
             System.out.println("Welcome Guest!");
             System.out.print("Do you want to play on easy - 0, medium - 1, or hard - 2? (0-2) ");
             int userDifficulty = obj.nextInt();
             System.out.println("You choose " + userDifficulty);
-           
+
             String[] levels = {
-                "Easy - User wins if you guess higher or same as Computer", 
-                "Medium - User wins if you guess higher than computer",
-                "Hard - User wins if you guess exactly as computer"};
+                    "Easy - User wins if you guess higher or same as Computer",
+                    "Medium - User wins if you guess higher than computer",
+                    "Hard - User wins if you guess exactly as computer" };
 
             System.out.println(levels[userDifficulty]);
 
-
-            if(userDifficulty > 2 || userDifficulty < 0) {
+            if (userDifficulty > 2 || userDifficulty < 0) {
                 System.out.println("Pick a number within range!");
                 return;
             }
 
             System.out.print("Pick a number (0-10): ");
             int userNumber = obj.nextInt();
-            
-            if(userNumber == value && userDifficulty == 0) {
+
+            if (userNumber == value && userDifficulty == 0) {
                 System.out.println("Your answer is the same. You Won!");
-            }
-            else if (userNumber == value && userDifficulty == 1) {
+            } else if (userNumber == value && userDifficulty == 1) {
                 System.out.println("Your answer is the same. Computer Wins!");
-            }
-            else if (userNumber < value) {
+            } else if (userNumber < value) {
                 System.out.println("Your answer was too low. Computer Wins!");
-            } 
-           else  if (userNumber > 10 && userNumber > value) {
+            } else if (userNumber > 10 && userNumber > value) {
                 System.out.println("Number outside range");
-            }
-            else if(userNumber > value && userDifficulty == 2) {
+            } else if (userNumber > value && userDifficulty == 2) {
                 System.out.println("Your answer was too high. Computer Wins!");
-            }
-            else {
+            } else {
                 System.out.println("Your answer was higher. You Win!");
             }
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             System.out.println("Type a postive number within range!");
         }
     }
