7751e24cbc1138b29f81cb3708395123d757b576
Abdella <abdella.shukur2@gmail.com>
2022-06-24 13:39:19 -0400

    added ASCII art

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,42 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Main {
     public static void main(String[] args) {
             int min = 0;
             int max = 10;
-    
+            int width = 100;
+            int height = 20;
+
+            // Buffered image 
+            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            Graphics g = image.getGraphics();
+            g.setFont(new Font("TimesNewRoman", Font.BOLD, 10));
+
+            Graphics2D graphics = (Graphics2D) g;
+            graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+            RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+            graphics.drawString("Guess the #", 20, 20);      
+            
+           //ImageIO.write(image, "png", new File("/users/mkyong/ascii-art.png"));
+
+        for (int y = 0; y < height; y++) {
+            StringBuilder sb = new StringBuilder();
+            for (int x = 0; x < width; x++) {
+
+                sb.append(image.getRGB(x, y) == -16777216 ? " " : "#");
+
+            }
+
+            if (sb.toString().trim().isEmpty()) {
+                continue;
+            }
+
+            System.out.println(sb);
+        }
+
             Random random = new Random();
             int value = random.nextInt(max+min)+min;
     
